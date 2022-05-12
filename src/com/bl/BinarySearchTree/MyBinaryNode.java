377f8b5becf4050250573169aa8c9cc9032160ba
shivajighasti04 <shivajighasti97@gamil.com>
2022-05-12 10:59:14 +0530

    ADDED: Class for Nodes of the BST

--- src/com/bl/BinarySearchTree/MyBinaryNode.java
+++ src/com/bl/BinarySearchTree/MyBinaryNode.java
@@ -1,7 +1,12 @@
 package com.bl.BinarySearchTree;
+/**
+ * 
+ * @author Shivaji
+ *
+ *
+ */
 
 public class MyBinaryNode<K extends Comparable<K>> {
-
 	K key;
 	MyBinaryNode<K> left;
 	MyBinaryNode<K> right;
@@ -27,3 +32,4 @@ public class MyBinaryNode<K extends Comparable<K>> {
 	}
 	
 }
+
