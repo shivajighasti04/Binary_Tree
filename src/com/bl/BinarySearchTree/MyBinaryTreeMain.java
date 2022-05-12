296f3c4b225f258107733281a2ba43abdad2b1ad
shivajighasti04 <shivajighasti97@gamil.com>
2022-05-12 10:36:51 +0530

    Adding BST

---
+++ src/com/bl/BinarySearchTree/MyBinaryTreeMain.java
@@ -0,0 +1,19 @@
+package com.bl.BinarySearchTree;
+
+public class MyBinaryTreeMain {
+public static void main(String[] args) {
+		
+		System.out.println("------ Welcome to Data Structure : Binary Search Tree ------");
+		
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		
+		myBinaryTree.printBinaryTree();
+
+	}
+
+}
+
+
